e4127ee8a738586840c702937fc8c1ae0d15d6ea
Elyse Burmester <burmesterelyse@gmail.com>
2019-09-04 22:50:52 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
+	generateMovieCards = () => {
+		return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+	};
 
- generateMovieCards = () => {
-    return movieData.map(data => {
-      return (
-        <div>
-          <MovieCard 
-            movieTitle={data.title} 
-            movieRating={data.IMDBRating}
-            movieGenres={data.genres}
-            moviePoster={data.poster} />
-        </div>
-      )
-  })
- }
-
-  render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
-  }
+	render() {
+		return <div id="movie-showcase">{this.generateMovieCards()}</div>;
+	}
 }
